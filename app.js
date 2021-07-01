aedf21e282370769622533805af6cc7e6d68b120
divyanshdahiya <dahiya.divyansh1@gmail.com>
2021-07-02 00:55:01 +0530

    0.5(api call)

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 // api key : 021b6592f1d6cd88cabae5596138c71e
 
-const kelvin = 273
-
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
 
-function displayWeather(){  
-    iconElement.innerHTML=` <img src="./icons/${weather.iconId}.png" alt="weather-icon"></img>`
-    tempElement.innerHTML= `${weather.temperature.value}°<span>C</span>`
-    descElement.innerHTML=`weather.description`
-    locationElement.innerHTML= `${weather.city}, ${weather.country}`
+
+
+
+const weather = {};
+
+weather.temperature = {
+    unit : "celsius"
 }
+
+
+
+
+
 if('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError)
 }
@@ -25,7 +30,7 @@ else{
 function setPosition (position){
     let latitude = position.coords.latitude
     let longitude = position.coords.longitude
-
+   
         getWeather(latitude, longitude)
 }
 
@@ -33,3 +38,59 @@ function showError (error){
     notificationElement.style.display= "block"
     notificationElement.innerHTML= `<p> ${error.message} </p>`
 }
+
+
+
+
+const kelvin = 273  
+const key = "021b6592f1d6cd88cabae5596138c71e"  
+
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            console.log(data    )
+            weather.temperature.value = Math.floor(data.main.temp - kelvin);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+function displayWeather(){  
+    iconElement.innerHTML=` <img src="./icons/${weather.iconId}.png" alt="weather-icon"></img>`
+    tempElement.innerHTML= `${weather.temperature.value}°<span>C</span>`
+    descElement.innerHTML=`${weather.description}`
+    locationElement.innerHTML= `${weather.city}, ${weather.country}`
+}
+
+
+
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
